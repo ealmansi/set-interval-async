b30b648c19d1e7cc1afa8d978415606e9c20b00e
EyePulp <eyepulp@eyepulp.net>
2021-05-13 12:44:11 -0500

    better spacing around an import

--- src/timer.js
+++ src/timer.js
@@ -3,7 +3,9 @@
  * This work is licensed under the terms of the MIT license.
  * For a copy, see the file LICENSE in the root directory.
  */
+
 import { clearIntervalAsync } from './clear'
+
 /**
  * Timer object returned by setIntervalAsync.<br>
  * Can be used together with {@link clearIntervalAsync} to stop execution.
